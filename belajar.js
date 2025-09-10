f587c298fc793b10ed1ccfb0a021b6a987e0a8b2
fauzan <fauzanss2021@gmail.com>
2025-09-10 14:53:33 +0700

    belajar jawascript

---
+++ belajar.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    alert("selamat datang di website fauzan irfanto");
+};
+confirm("apakah anda ingin melanjutkan");
+prompt("saya belajar");
+Date(10)
+perseInt("123")
+setTimeout(function() {
+    alert("saya pemula.");
+}, 2000);
+function foo() {
+alert("aur function just ran!");}
